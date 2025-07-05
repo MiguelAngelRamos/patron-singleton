ac5ec36f438ec73c6fcf3c60d54bd300cd783816
Miguel Ramos <return.infox@gmail.com>
2025-07-05 11:13:06 -0400

    singleton terminado

--- src/App.java
+++ src/App.java
@@ -1,3 +1,20 @@
+import db.DatabaseConecction;
+
 public class App {
   
+  public static void main(String[] args) {
+    DatabaseConecction databaseConecctionUno = DatabaseConecction.getInstance();
+    DatabaseConecction databaseConecctionDos = DatabaseConecction.getInstance();
+
+    System.out.println("databaseConnectionUno" + databaseConecctionUno.getConnection());
+    System.out.println("databaseConnectionDos" + databaseConecctionDos.getConnection());
+
+  // = asignacion
+  // == comparación
+    if(databaseConecctionUno == databaseConecctionDos) {
+      System.out.println("Ambas instancias son iguales (Sigleton Funciona)");
+    } else {
+       System.out.println("Las instancias son diferentes (Sigleton No Funciona)");
+    }
+  }
 }
